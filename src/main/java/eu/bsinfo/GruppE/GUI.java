4f1ced4a9946350aeded003f83bf29b3f53dcaed
Felix Schebesta <felix.schebesta@firum.de>
2022-10-17 11:50:25 +0200

    added basic GUI stuff

--- src/main/java/eu/bsinfo/GruppE/GUI.java
+++ src/main/java/eu/bsinfo/GruppE/GUI.java
@@ -1,7 +1,30 @@
 package eu.bsinfo.GruppE;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class GUI extends JFrame {
+    public GUI() {
+        super("Zählerabrechnung - ProgSchnellUndSicher GmbH");
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                exit();
+            }
+        });
+
+        final Container con = getContentPane();
+        con.setLayout(new BorderLayout());
+        final JPanel pn = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JTextField customerId;
+        pn.add(customerId = new JTextField(40));
+
+    }
+
+    private void exit() {
+        System.exit(0);
+    }
 
 }
