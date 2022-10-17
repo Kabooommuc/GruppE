a5405162ac3fd5e658f829a6fe4b48cf21a072e7
Felix Schebesta <kaboommuc@gmail.com>
2022-10-17 14:41:44 +0200

    Input fields: right aligned; more padding

--- src/main/java/eu/bsinfo/GruppE/GUI.java
+++ src/main/java/eu/bsinfo/GruppE/GUI.java
@@ -3,6 +3,7 @@ package eu.bsinfo.GruppE;
 import eu.bsinfo.GruppE.DataClasses.MeasurementData;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -17,6 +18,8 @@ public class GUI extends JFrame {
     JTextField measurementReadingDateInput = new JTextField();
     JCheckBox counterChangeInput = new JCheckBox();
     JTextField commentInput = new JTextField();
+    JTextField powerCurrentInput = new JTextField();
+    JTextField householdElecticityInput = new JTextField();
     public GUI() {
         super("Zählerabrechnung - ProgSchnellUndSicher GmbH");
         addWindowListener(new WindowAdapter() {
@@ -30,16 +33,39 @@ public class GUI extends JFrame {
         base.setLayout(new GridBagLayout());
 
         final Container inputFields = new Container();
-        inputFields.setLayout(new GridLayout(2, 8));
+        inputFields.setLayout(new GridLayout(2, 10));
 
-        JLabel customerIdLabel = new JLabel("KundenNr");
-        JLabel houseNumberLabel = new JLabel("HausNr");
-        JLabel apartmentNumberLabel = new JLabel("WohnungsNr");
-        JLabel counterTypeLabel = new JLabel("Zählerart");
-        JLabel counterIdLabel = new JLabel("ZählerID");
-        JLabel measurementReadingDateLabel = new JLabel("Ablesedatum");
-        JLabel counterChangeLabel = new JLabel("Zählertausch");
-        JLabel commentLabel = new JLabel("Kommentar");
+
+        JLabel customerIdLabel = new JLabel("KundenNr ");
+        customerIdLabel.setHorizontalAlignment(JLabel.RIGHT);
+        customerIdLabel.setBorder(new EmptyBorder(0,15,0,10));
+        JLabel houseNumberLabel = new JLabel("HausNr ");
+        houseNumberLabel.setHorizontalAlignment(4); // JLabel.Right = 4
+        houseNumberLabel.setBorder(new EmptyBorder(0,15,0,10));
+        JLabel apartmentNumberLabel = new JLabel("WohnungsNr ");
+        apartmentNumberLabel.setHorizontalAlignment(4);
+        apartmentNumberLabel.setBorder(new EmptyBorder(0,15,0,10));
+        JLabel counterTypeLabel = new JLabel("Zählerart ");
+        counterTypeLabel.setHorizontalAlignment(4);
+        counterTypeLabel.setBorder(new EmptyBorder(0,15,0,10));
+        JLabel counterIdLabel = new JLabel("ZählerID ");
+        counterIdLabel.setHorizontalAlignment(4);
+        counterIdLabel.setBorder(new EmptyBorder(0,15,0,10));
+        JLabel measurementReadingDateLabel = new JLabel("Ablesedatum ");
+        measurementReadingDateLabel.setHorizontalAlignment(4);
+        measurementReadingDateLabel.setBorder(new EmptyBorder(0, 15,0,10));
+        JLabel counterChangeLabel = new JLabel("Zählertausch ");
+        counterChangeLabel.setHorizontalAlignment(4);
+        counterChangeLabel.setBorder(new EmptyBorder(0, 15,0, 10));
+        JLabel commentLabel = new JLabel("Kommentar ");
+        commentLabel.setHorizontalAlignment(4);
+        commentLabel.setBorder(new EmptyBorder(0, 15, 0, 10));
+        JLabel powerCurrentLabel = new JLabel("Kraftstrom ");
+        powerCurrentLabel.setHorizontalAlignment(4);
+        powerCurrentLabel.setBorder(new EmptyBorder(0, 15, 0, 10));
+        JLabel householdElectricityLabel = new JLabel("Haushaltsstrom ");
+        householdElectricityLabel.setHorizontalAlignment(4);
+        householdElectricityLabel.setBorder(new EmptyBorder(0,15,0,10));
 
         inputFields.add(customerIdLabel);
         inputFields.add(customerIdInput);
@@ -47,8 +73,10 @@ public class GUI extends JFrame {
         inputFields.add(houseNumberInput);
         inputFields.add(apartmentNumberLabel);
         inputFields.add(apartmentNumberInput);
-        inputFields.add(counterChangeLabel);
-        inputFields.add(counterChangeInput);
+        inputFields.add(powerCurrentLabel);
+        inputFields.add(powerCurrentInput);
+        inputFields.add(householdElectricityLabel);
+        inputFields.add(householdElecticityInput);
 
         inputFields.add(counterTypeLabel);
         inputFields.add(counterTypeInput);
@@ -58,6 +86,8 @@ public class GUI extends JFrame {
         inputFields.add(measurementReadingDateInput);
         inputFields.add(commentLabel);
         inputFields.add(commentInput);
+        inputFields.add(counterChangeLabel);
+        inputFields.add(counterChangeInput);
 
         base.add(inputFields);
 
@@ -70,8 +100,9 @@ public class GUI extends JFrame {
         final Container actionButtons = new Container();
         actionButtons.setLayout(new GridBagLayout());
 
-        setSize(800, 100);
+        setSize(1450, 100);
         setVisible(true);
+
 
     }
 
