069c2af1128f9c44c104059c946059639bf45665
jesslyn.grossmann <jo.grossmann@arcor.de>
2022-10-18 13:08:21 +0200

    Save Data

--- src/main/java/eu/bsinfo/GruppE/DataHandler.java
+++ src/main/java/eu/bsinfo/GruppE/DataHandler.java
@@ -2,14 +2,29 @@ package eu.bsinfo.GruppE;
 
 import lombok.Getter;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
+import static eu.bsinfo.GruppE.Runtime.gui;
+
 public class DataHandler {
+    private static final String SAVE_FILENAME = "data";
+    private static final String ERROR_WHILE_SAVING = "Error: File failed to save";
     @Getter
     public static ArrayList<MeasurementData> data = new ArrayList<>();
 
     public static void addData(MeasurementData d) {
         data.add(d);
+    }
+
+    public static void saveData() {
+        try {
+            DataExporter.exportJSON(data, SAVE_FILENAME);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            gui.setErrorMessage(ERROR_WHILE_SAVING);
+        }
     }
 
 }
--- src/main/java/eu/bsinfo/GruppE/Runtime.java
+++ src/main/java/eu/bsinfo/GruppE/Runtime.java
@@ -2,8 +2,9 @@ package eu.bsinfo.GruppE;
 
 public class Runtime {
 
+    public static GUI gui;
     public static void main(String[] args) {
-        new GUI();
+        gui = new GUI();
     }
 
 }
