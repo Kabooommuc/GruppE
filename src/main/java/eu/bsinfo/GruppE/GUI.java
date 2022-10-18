6dd173e9bc16aec4eba5db25f2d4e0157b4dc920
Felix Schebesta <kaboommuc@gmail.com>
2022-10-18 13:03:01 +0200

    GUI: Added ActionButtons

--- src/main/java/eu/bsinfo/GruppE/GUI.java
+++ src/main/java/eu/bsinfo/GruppE/GUI.java
@@ -48,8 +48,14 @@ public class GUI extends JFrame {
             }
         });
 
+
         final Container base = getContentPane();
+        final boolean shouldFill = true;
+        GridBagConstraints c = new GridBagConstraints();
         base.setLayout(new GridBagLayout());
+        if (shouldFill) {
+            c.fill = GridBagConstraints.HORIZONTAL;
+        }
 
         final Container inputFields = new Container();
         inputFields.setLayout(new GridLayout(2, 10));
@@ -65,15 +71,15 @@ public class GUI extends JFrame {
         JLabel commentLabel = new JLabel("Kommentar ");
         JLabel counterChangeLabel = new JLabel("Zählertausch ");
 
-        // this array exists for the loop, which sets the padding and the right alignment
+            // this array exists for the loop, which sets the padding and the right alignment
         JLabel[] list = {
                 customerIdLabel, houseNumberLabel, apartmentNumberLabel,
                 powerCurrentLabel, householdCurrentLabel, counterTypeLabel,
                 counterIdLabel, commentLabel, counterChangeLabel, measurementReadingDateLabel
         };
-        for ( JLabel j : list ) {
+        for (JLabel j : list) {
             j.setHorizontalAlignment(4);
-            j.setBorder(new EmptyBorder(0,15,0,10));
+            j.setBorder(new EmptyBorder(0, 15, 0, 10));
         }
 
         inputFields.add(customerIdLabel);
@@ -99,24 +105,54 @@ public class GUI extends JFrame {
         inputFields.add(counterChangeLabel);
         inputFields.add(counterChangeInput);
 
-        base.add(inputFields);
+        c.gridx = 0;
+        c.gridy = 0;
+        base.add(inputFields, c);
 
         final Container dataScrollpane = new Container();
         dataScrollpane.setLayout(new ScrollPaneLayout());
 
-        final Container dataGrid = new Container();
-        dataGrid.setLayout(new GridBagLayout());
-
         // error messages should be written into this container
         final Container errorLog = new Container();
-        JLabel logging = new JLabel();
+        
+//        base.add(errorMessageLabel);
+
+
 
         final Container actionButtons = new Container();
-        actionButtons.setLayout(new GridBagLayout());
+        GridBagLayout aBgbl = new GridBagLayout();              // actionButtonsGridBagLayout
+        GridBagConstraints aBgbc = new GridBagConstraints();    // actionButtonsGridBagConstraints
+        actionButtons.setLayout(aBgbl);
+        aBgbl.setConstraints(actionButtons, aBgbc);
 
-        setSize(1450, 100);
+        JButton addButton = new JButton("Add");
+        JButton saveButton = new JButton("Save");
+        JButton exportButton = new JButton("Export");
+        JButton exitButton = new JButton("Exit");
+
+        aBgbc.gridx = 0;
+        actionButtons.add(addButton);
+        aBgbc.gridx = 1;
+        actionButtons.add(saveButton);
+        aBgbc.gridx = 3;
+        actionButtons.add(exportButton);
+        aBgbc.gridx = 4;
+        aBgbc.anchor = GridBagConstraints.EAST;
+        actionButtons.add(exitButton);
+
+        c.gridx = 0;
+        c.gridy = 1;
+        base.add(actionButtons, c);
+
+        setSize(1500, 150);
         setVisible(true);
+
+        addButton.addActionListener(e -> addData());
+//        saveButton.addActionListener(e -> save());
+//        exportButton.addActionListener(e -> export());
+        exitButton.addActionListener(e -> exit());
     }
+
 
     public void addData() {
         MeasurementData m;
