2a361e6d6071448f287131ddcf9f022b04b1d0d9
Felix Schebesta <kaboommuc@gmail.com>
2023-02-16 10:48:17 +0100

    Remove unused code in Server.java

--- src/main/java/eu/bsinfo/GruppE/Server/Server.java
+++ src/main/java/eu/bsinfo/GruppE/Server/Server.java
@@ -1,40 +1,26 @@
 package eu.bsinfo.GruppE.Server;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.sun.net.httpserver.HttpServer;
-import eu.bsinfo.GruppE.Server.models.Ablesung;
-import eu.bsinfo.GruppE.Server.ressources.AblesungRessource;
-import eu.bsinfo.GruppE.Server.ressources.KundenRessource;
-import eu.bsinfo.GruppE.Server.ressources.UUIDRessource;
+import eu.bsinfo.GruppE.Server.Database.Util;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URI;
-import java.nio.file.Paths;
+import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.UUID;
 
 import static eu.bsinfo.GruppE.Server.Database.setupDatabase.createDB;
 
 public class Server {
 
-    private static final File targetDirectoryFile = Paths.get("target").toFile();
-    private static final String serverAblesungenFileName = "server_ablesungen";
-    private static final String serverKundenFileName = "server_kunden";
-    private static final String serverUUIDFileName = "server_uuid";
-    private static final String serveridFileName = "server_id";
     final static String pack = "eu.bsinfo.GruppE.Server.ressources";
     static HttpServer server;
     final static ResourceConfig rc = new ResourceConfig().packages(pack);
+    static Connection con = Util.getConnection("gm3");
 
-   // static Connection con = Util.getConnection("gm3");
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String url = "http://localhost:8080/rest";
         startServer(url, true);
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
@@ -46,21 +32,9 @@ public class Server {
         }, "Shutdown-thread"));
     }
 
-    public static void startServer(String url, boolean loadFromFile) throws IOException {
+    public static void startServer(String url, boolean loadFromFile) {
         System.out.println("Start server");
         System.out.println(url);
-
-        if(loadFromFile){
-            System.out.println("Loading from files...");
-
-            ObjectMapper objMapper = new ObjectMapper()
-                    .registerModule(new JavaTimeModule());
-            File jsonFileAblesungen = new File(targetDirectoryFile + "/" + serverAblesungenFileName + ".json");
-            if(jsonFileAblesungen.exists()) {
-                Ablesung[] importedArrayData = objMapper.readValue(jsonFileAblesungen, Ablesung[].class);
-                AblesungRessource.ablesungen = new ArrayList<>(Arrays.asList(importedArrayData));
-            }
-        }
 
         server = JdkHttpServerFactory.createHttpServer(URI.create(url), rc);
 
@@ -75,28 +49,14 @@ public class Server {
 
     public static void stopServer(boolean saveToFile) throws IOException {
         if(saveToFile) {
-            System.out.println("Saving to files...");
-            saveData(AblesungRessource.ablesungen, serverAblesungenFileName);
-            saveData(KundenRessource.kunden, serverKundenFileName);
-            saveData(UUIDRessource.idUUIDpairs, serverUUIDFileName);
-            saveData(UUIDRessource.UUIDidpairs, serveridFileName);
+            System.out.println("Server shutdown...");
         }
 
         if(server == null || server.getExecutor() == null )
             return;
 
-      //  Util.close(con);
+        Util.close(con);
         server.stop(0);
-    }
-
-    private static void saveData(Object dataToExport, String fileNameNoSuffix) throws IOException {
-        File jsonFile = new File(targetDirectoryFile + "/" + fileNameNoSuffix + ".json");
-
-        ObjectMapper objMapper = new ObjectMapper()
-                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-                .configure(SerializationFeature.INDENT_OUTPUT, true)
-                .registerModule(new JavaTimeModule());
-        objMapper.writeValue(jsonFile, dataToExport);
     }
 
     public static UUID convertStringToUUID(String strUUID) {
